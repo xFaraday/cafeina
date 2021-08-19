5faf0953dc03c6d2fc02f4b495adf036818ce5be
xfaraday <theonlyfaraday@gmail.com>
2021-08-18 20:56:12 -0700

    CSV Parser done :)

--- data-flow/sql-controller.py
+++ data-flow/sql-controller.py
@@ -2,14 +2,11 @@
 import mysql.connector
 import csv
 
-#database
-#tables
-#values?
-
 def add_to_key(dictionary, key, vals):
 	if key not in dictionary:
 		dictionary[key] = vals
-	dictionary[key].extend(vals)
+	else:
+		dictionary[key].extend(vals)
 	return dictionary
 
 def get_vals_from_val_list(num,r_l,vals):
@@ -20,13 +17,11 @@ def get_vals_from_val_list(num,r_l,vals):
 
 def form_dict(rows, columns):
 	dict = {}
-	print(columns, rows)
 	col_len=len(columns)
 	row_len=len(rows)
 	for i in range(0,col_len):
 		bruh = get_vals_from_val_list(i, row_len, rows)
-		print(bruh)
-		#add_to_key(dict, columns[i], bruh)
+		add_to_key(dict, columns[i], bruh)
 	print(dict)
 
 def parse_csv():
@@ -42,30 +37,6 @@ def parse_csv():
 			else:
 				rows.append(row)
 		form_dict(rows,columns)
-
-	#print("cols"+str(cols))
-	#print(columns[cols])
-	#	for cols in range(0, len(columns)):
-	#		for row in csv_reader:
-	#			print("hello")
-	#			print(row)
-	#print("\titem"+str(item))
-
-
-	#	print("Number of columns " + str(len(columns)))
-	#	print("Number of lines " + str(line_count))
-
-
-#if line_count == 0:
-#	print(row[line_count])
-#else:
-#	print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-#	line_count+=1
-
-#add_row = ("INSERT INTO testing"
-#			"(first, second, third,"
-#
-#	)
 
 def add_data():
 	print("add the data mate")
